44ef95d8e24ffa7b63b8595150716826236e7ad7
Thorbjørn Bonvik <thorbjrn@student.chalmers.se>
2016-10-28 01:01:48 +0200

    add counter and update frame with scene

--- ThorBoard.js
+++ ThorBoard.js
@@ -15,20 +15,30 @@ context.moveTo(0,240);
 context.lineTo(720,240);
 context.stroke();
 
+var score = 0
+var gameStarted = false;
+
 // execute main function now
 main();
 
 //Functions:
 
 function main(){
-  var text = displayText("hello world. press space to play or something");
+  var text = "hello world. press space to play or something";
+  displayText(text);
 
   skater.onload = function(){
     context.drawImage(skater, 50, 93);
   }
 
+
+
   onkeypress = function(){
-    setInterval("update()",1000/60); //60 Frames per second
+    //var key = event.key;
+    if (gameStarted == false){
+      setInterval("drawFrame()", 1000/60); //60 Frames per second
+      gameStarted = true;
+    }
   }
 
 }
@@ -38,14 +48,17 @@ function displayText(text) {
   context.strokeText(text,10,50);
 }
 
-function update(){
+function drawFrame(){
     context.clearRect(0,0,canvas.width,canvas.height);
-    context.fillText(count,200,200);
     //Draw anything else...
     displayText(score);
+    score++;
 
-    skater.onload = function(){
-      context.drawImage(skater, 50, 93);
-    }
+    context.drawImage(skater, 50, 93);
+
+    context.moveTo(0,240);
+    context.lineTo(720,240);
+    context.stroke();
+
 
 }
