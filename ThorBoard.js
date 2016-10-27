925fabf21a0c0224ce91d62ea8bf4eef179eb7bf
Thorbjørn Bonvik <thorbjrn@student.chalmers.se>
2016-10-27 20:14:36 +0200

    Add hello world

---
+++ ThorBoard.js
@@ -0,0 +1,10 @@
+// JavaScript test 2016-10-27
+
+//ThorBoard
+//A simple skateboarding game
+
+console.log("Hello world");
+
+
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
