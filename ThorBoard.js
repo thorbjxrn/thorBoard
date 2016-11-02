d9acf32ed3e2c19678442ce2bd074f8cade0e51a
Thorbjørn Bonvik <thorbjrn@student.chalmers.se>
2016-11-02 21:45:58 +0100

    Add charging before jump, and slimmer skateboarder

--- ThorBoard.js
+++ ThorBoard.js
@@ -10,12 +10,16 @@ var context = canvas.getContext('2d');
 
 var skater = new Image();
 skater.src = 'resources/skater.png';
+
 var skaterPosDefault = [50,93];
 var skaterPos = skaterPosDefault;
 var skaterInAir = false;
-var jumpTime = 15;
+var jumpHeight = 15;
+var jumpHeightTotal = jumpHeight;
+var jumpHeightMax = 60;
 var jumpTimer = 0;
 
+var downKeyIsDown = false;
 
 context.moveTo(0,240);
 context.lineTo(720,240);
@@ -42,66 +46,94 @@ function main(){
   function keyPressHandler(event)
   {
   	var keyPressed = event.keyCode;
-
-  	if (keyPressed == 38)
-  	{
-  		    console.log("up");
-
-          if(skaterInAir == false){
-            skaterPos = [51, 80];
-            skaterInAir = true;
-            jumpTimer = jumpTime;
-          }
-  	}
-  	else if (keyPressed == 40)
-  	{
-        console.log("down");
-  	}
+    if(gameStarted == true){
+    	if (keyPressed == 38)
+    	{
+    		    console.log("up");
+            if(skaterPos == skaterPosDefault){
+              skaterInAir = true;
+              console.log("jump!");
+            }
+    	}
+    	else if (keyPressed == 40)
+    	{
+          console.log("down");
+          downKeyIsDown = true;
+    	}
+    }
   }
+
+function keyUpHandler(event){
+  var keyPressed = event.keyCode;
+  if(gameStarted == true){
+    if (keyPressed == 40){
+        downKeyIsDown = false;
+    }
+  }
+}
 
 
   document.addEventListener("keydown",keyPressHandler, false);
-
+  document.addEventListener("keyup",keyUpHandler, false);
 
   onkeypress = function(){
-    //var key = event.key;
-
     if (gameStarted == false){
       setInterval("drawFrame()", 1000/60); //60 Frames per second
       gameStarted = true;
     }
-  }
+  }// end key handling
 
-}
+} //end of main
 
 function displayText(text) {
   context.font = "30px Arial";
   context.strokeText(text,10,50);
 }
 
+function drawPlayer(){
+  if(skaterInAir){
+    jumpTimer++;
+    if(jumpTimer < jumpHeightTotal){
+      skaterPos[1]--;
+    }
+    else if (jumpTimer == jumpHeightTotal){
+      score++;
+      console.log(jumpTimer + " jumpTimer,");
+    }
+    else if (jumpTimer > jumpHeightTotal && jumpTimer < jumpHeightTotal*2){
+      skaterPos[1]++;
+    }
+    else {
+      skaterInAir = false;
+      jumpTimer = 0;
+    }
+  }
+  else if(downKeyIsDown && (jumpHeightTotal < jumpHeightMax)){
+    jumpHeightTotal++;
+  }
+  else if(!downKeyIsDown) {
+    if(jumpHeightTotal > jumpHeight){
+      jumpHeightTotal--;
+    }
+  }
+
+
+  context.drawImage(skater, skaterPos[0], skaterPos[1]);
+
+}
+
 function drawFrame(){
     context.clearRect(0,0,canvas.width,canvas.height);
     //Draw anything else...
-    displayText(score);
+    displayText(jumpHeightTotal);
     score++;
-
-    if(skaterInAir){
-      if(jumpTimer < 1){
-        skaterInAir = false;
-        skaterPos = skaterPosDefault;
-      }
-      else{
-        jumpTimer--;
-        skaterPos[1]++;
-      }
-
-    }
-
-    context.drawImage(skater, skaterPos[0], skaterPos[1]);
 
     context.moveTo(0,240);
     context.lineTo(720,240);
     context.stroke();
+
+
+    drawPlayer();
 
 
 }
